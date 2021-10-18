d0b0207613472ef8d68240864a7e8758612ef547
Moonchild <moonchild@palemoon.org>
2021-10-18 16:19:08 +0000

    Issue #2 - Remove Safebrowsing and TrackingProtection from the FE
    This resolves #2

--- basilisk/base/content/browser.js
+++ basilisk/base/content/browser.js
@@ -51,11 +51,6 @@ Cu.import("resource://gre/modules/NotificationDB.jsm");
   ["webrtcUI", "resource:///modules/webrtcUI.jsm", ]
 ].forEach(([name, resource]) => XPCOMUtils.defineLazyModuleGetter(this, name, resource));
 
-#ifdef MOZ_SAFE_BROWSING
-  XPCOMUtils.defineLazyModuleGetter(this, "SafeBrowsing",
-    "resource://gre/modules/SafeBrowsing.jsm");
-#endif
-
 // lazy service getters
 [
   ["Favicons", "@mozilla.org/browser/favicon-service;1", "mozIAsyncFavicons"],
@@ -955,7 +950,6 @@ var gBrowserInit = {
     BrowserOnClick.init();
     FeedHandler.init();
     AboutPrivateBrowsingListener.init();
-    TrackingProtection.init();
     RefreshBlocker.init();
     CaptivePortalWatcher.init();
 
@@ -1167,11 +1161,6 @@ var gBrowserInit = {
         loadOneOrMoreURIs(uriToLoad);
       }
     }
-
-#ifdef MOZ_SAFE_BROWSING
-    // Bug 778855 - Perf regression if we do this here. To be addressed in bug 779008.
-    setTimeout(function() { SafeBrowsing.init(); }, 2000);
-#endif
 
     Services.obs.addObserver(gIdentityHandler, "perm-changed", false);
     Services.obs.addObserver(gSessionHistoryObserver, "browser:purge-session-history", false);
@@ -1475,8 +1464,6 @@ var gBrowserInit = {
     BrowserOnClick.uninit();
 
     FeedHandler.uninit();
-
-    TrackingProtection.uninit();
 
     RefreshBlocker.uninit();
 
@@ -4292,7 +4279,6 @@ var XULBrowserWindow = {
       uri = Services.uriFixup.createExposableURI(uri);
     } catch (e) {}
     gIdentityHandler.updateIdentity(this._state, uri);
-    TrackingProtection.onSecurityChange(this._state, aIsSimulated);
   },
 
   // simulate all change notifications after switching tabs
@@ -7741,12 +7727,6 @@ var AboutPrivateBrowsingListener = {
       "AboutPrivateBrowsing:OpenPrivateWindow",
       msg => {
         OpenBrowserWindow({private: true});
-    });
-    window.messageManager.addMessageListener(
-      "AboutPrivateBrowsing:ToggleTrackingProtection",
-      msg => {
-        const PREF = "privacy.trackingprotection.pbmode.enabled";
-        Services.prefs.setBoolPref(PREF, !Services.prefs.getBoolPref(PREF));
     });
   }
 };
