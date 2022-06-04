ef0c5077da9a1eec8f07f76f7bea2bb924357733
Brian Smith <brian@dbsoft.org>
2022-06-04 14:16:01 -0500

    Revert "No issue - Explicitly enable bundled fonts for building from platform release branch"
    This reverts commit 262b3061f85db28a79413284e2a8bab89b5bbbbd.

--- basilisk/confvars.sh
+++ basilisk/confvars.sh
@@ -9,7 +9,6 @@ MOZ_PHOENIX=1
 MOZ_AUSTRALIS=1
 MC_BASILISK=1
 MOZ_UPDATER=
-MOZ_BUNDLED_FONTS=1
 
 # For Basilisk we want to use 52.9.YYYY.MM.DD as MOZ_APP_VERSION in release
 # builds so add-on developers have something to target while maintaining
