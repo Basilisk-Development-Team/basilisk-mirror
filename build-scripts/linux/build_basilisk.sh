b84d482544ec7b4dfad205087b48a47b73852d45
Basilisk-Dev <basiliskdev@protonmail.com>
2024-02-03 15:03:28 -0500

    Make build script universal to x86 and arm. Switch to Oracle Linux 8 with GCC 11.

---
+++ build-scripts/linux/build_basilisk.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ ! $(which docker) ]; then
+    echo "Docker must be installed to run this script."
+    exit
+fi
+
+if [ $(uname -s) != "Linux" ]; then
+    echo "This script is only meant to be ran on Linux."
+    exit
+fi
+
+# This probably isn't the best way to determine if we are in the Basilisk root directory but it works for now.
+if [ ! -f aclocal.m4 ]; then
+    echo "This script must be ran from the root of the Basilisk codebase."
+    exit
+fi
+
+if [ ! -f .mozconfig ]; then
+    if [ $(uname -m) = "x86_64" ]; then
+        cp mozconfigs/linux/x86_64/gtk3_unofficial_branding.mozconfig .mozconfig
+    elif [ $(uname -m) = "aarch64" ]; then
+        cp mozconfigs/linux/aarch64/gtk3_unofficial_branding.mozconfig .mozconfig
+    fi
+fi
+
+# If argument 1 is provided then apply patches before build
+APPLYPATCHES=$1
+
+echo "Starting Container..."
+
+docker run -i -v $PWD:/share --rm -e APPLYPATCHES=$APPLYPATCHES -e UID=$(id -u) -e GID=$(id -g) -e USERNAME=$(whoami) -e GROUPNAME=$(id -gn) -t oraclelinux:8 /share/build-scripts/linux-x86_64/build_basilisk_subscripts/run_inside_docker.sh
---
+++ build-scripts/linux/build_basilisk_subscripts/run_inside_docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Prepping user and group inside docker container..."
+groupadd -r -g $GID $GROUPNAME
+useradd -u $UID $USERNAME -g $GID
+
+ls -alh /share
+
+dnf install -y epel-release
+
+dnf config-manager --set-enabled ol8_codeready_builder
+
+dnf install -y gtk3-devel dbus-glib-devel GConf2-devel autoconf213 yasm \
+mesa-libGL-devel alsa-lib-devel libXt-devel openssl-devel sqlite-devel \
+pulseaudio-libs-devel python2 gtk2-devel
+
+dnf groupinstall -y 'Development Tools'
+
+dnf install -y gcc-toolset-11
+. /opt/rh/gcc-toolset-11/enable
+
+echo "Building Basilisk..."
+cd /share
+
+if [ "$APPLYPATCHES" = "yes" ]; then
+    patch -p1 < patches/0001-goanna-disable-pref.diff
+fi
+
+su -c "./mach clobber" $USERNAME
+su -c "./mach configure" $USERNAME
+su -c "./mach build" $USERNAME
+su -c "./mach package" $USERNAME
