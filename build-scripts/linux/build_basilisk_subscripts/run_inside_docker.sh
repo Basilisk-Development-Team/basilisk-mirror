bb0ce2dd27782bcd242905fcc067297d5918951b
Basilisk-Dev <basiliskdev@protonmail.com>
2024-09-05 18:09:06 -0400

    GitHub Actions - Install dependencies for 32-bit builds if building on x86_64

--- build-scripts/linux/build_basilisk_subscripts/run_inside_docker.sh
+++ build-scripts/linux/build_basilisk_subscripts/run_inside_docker.sh
@@ -15,6 +15,18 @@ python2 gtk2-devel
 dnf groupinstall -y 'Development Tools'
 
 dnf install -y gcc-toolset-11
+
+# Install dependencies to build an i686 build if building on x86_64
+if [ "$(uname -m)" = "x86_64" ]; then
+    dnf install -y glibc-devel.i686 libstdc++-devel.i686 gcc-toolset-11-libstdc++-devel.i686 \
+    fontconfig-devel.i686  freetype-devel.i686 gtk2-devel.i686 gtk3-devel.i686 libXt-devel.i686 \
+    libXext-devel.i686 libX11-devel.i686 libX11-xcb.i686 libxcb-devel.i686 glib2-devel.i686 \
+    pulseaudio-libs-devel.i686 GConf2-devel.i686 dbus-devel.i686 atkmm-devel.i686 \
+    libXrender-devel.i686 alsa-lib-devel.i686 atk-devel.i686  pango-devel.i686 dbus-glib-devel.i686 \
+    libXfixes-devel.i686 libXcomposite-devel.i686 libXdamage-devel.i686
+fi
+
+# Enable GCC 11
 . /opt/rh/gcc-toolset-11/enable
 
 echo "Building Basilisk..."
