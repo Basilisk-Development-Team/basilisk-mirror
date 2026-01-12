cde7b03db9c729f8faf4d0d639c9268702597da9
Basilisk-Dev <basiliskdev@protonmail.com>
2026-01-11 22:48:49 -0500

    forgejo test

---
+++ .forgejo/workflows/scripts/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+dnf install -y epel-release
+
+dnf config-manager --set-enabled ol8_codeready_builder
+
+dnf install -y gtk3-devel dbus-glib-devel GConf2-devel yasm mesa-libGL-devel \
+  alsa-lib-devel libXt-devel openssl-devel sqlite-devel pulseaudio-libs-devel \
+  python2 gtk2-devel python2-devel wget xz make cmake python2 which
+
+dnf groupinstall -y 'Development Tools'
+
+dnf install -y gcc-toolset-11 gcc-toolset-11-annobin*
+
+# Install dependencies to build an i686 build if building on x86_64
+if [ "$(uname -m)" = "x86_64" ]; then
+    dnf install -y glibc-devel.i686 libstdc++-devel.i686 gcc-toolset-11-libstdc++-devel.i686 \
+    fontconfig-devel.i686  freetype-devel.i686 gtk2-devel.i686 gtk3-devel.i686 libXt-devel.i686 \
+    libXext-devel.i686 libX11-devel.i686 libX11-xcb.i686 libxcb-devel.i686 glib2-devel.i686 \
+    pulseaudio-libs-devel.i686 GConf2-devel.i686 dbus-devel.i686 atkmm-devel.i686 \
+    libXrender-devel.i686 alsa-lib-devel.i686 atk-devel.i686  pango-devel.i686 dbus-glib-devel.i686 \
+    libXfixes-devel.i686 libXcomposite-devel.i686 libXdamage-devel.i686 gdk-pixbuf2-devel.i686 \
+    cairo-gobject-devel.i686 gcc-toolset-11*.i686 gcc-toolset-14*.i686
+fi
+
+# Clang toolchain
+dnf install -y clang llvm lld
+
+ln -s /usr/bin/python2 /usr/local/bin/python
+
+# Enable GCC 11
+. /opt/rh/gcc-toolset-11/enable
+
+# Check for mozconfig before building, add fallback if none present
+if [ ! -f .mozconfig ]; then
+    if [ $(uname -m) = "x86_64" ]; then
+        cp mozconfigs/linux/x86_64/gtk3_unofficial_branding.mozconfig .mozconfig
+    elif [ $(uname -m) = "aarch64" ]; then
+        cp mozconfigs/linux/aarch64/gtk3_unofficial_branding.mozconfig .mozconfig
+    fi
+fi
+
+./mach clobber
+./mach configure
+./mach build
+./mach package
