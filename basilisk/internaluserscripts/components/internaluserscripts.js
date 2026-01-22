61ff4891faebacff5a43f96cdf68a52821df4d56
Basilisk-Dev <basiliskdev@protonmail.com>
2026-01-22 16:32:31 -0500

    Update internaluserscripts.js to be MIT licensed to prevent any conflicts with greasemonkey license

--- basilisk/internaluserscripts/components/internaluserscripts.js
+++ basilisk/internaluserscripts/components/internaluserscripts.js
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+/* This source file is licensed under the MIT License.
+ * A copy of the MIT License should have been distributed with this
+ * file. If not, see https://opensource.org/licenses/MIT.
  */
 
 if (typeof Cc === "undefined") {
